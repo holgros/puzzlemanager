26c5193cd1c19c73c65dd32027c27c6ffd4706d0
Elev <holgros@gmail.com>
2022-06-16 23:54:57 +0200

    Fortsatt arbete på preview

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { setClassMetadata } from '@angular/core/src/r3_symbols';
 
 import { Puzzle } from '../puzzle';
 
@@ -8,12 +9,126 @@ import { Puzzle } from '../puzzle';
   styleUrls: ['./preview.component.css']
 })
 export class PreviewComponent implements OnInit {
+  
+  scale = Math.floor(window.innerWidth/12);
 
+  polygonStrings: string[] = [];
+  points = generatePoints(this.scale);
+  indices = getIndices(27);
+  styleStringLetters: string = `font: ${Math.round(this.scale/5)}px sans-serif; text-anchor: middle;`;
+  transformStringLetters: string[] = generateTransformationStrings(this.points);
+
+  Math = Math;
+  
   @Input() puzzle: Puzzle;
 
-  constructor() { }
+  constructor() {
+    for (let i = 0; i < 27; i+=3) {
+      this.polygonStrings.push(`${this.points.x[i]},${this.points.y[i]} ${this.points.x[i+1]},${this.points.y[i+1]} ${this.points.x[i+2]},${this.points.y[i+2]}`);
+    }
+    
+  }
 
   ngOnInit(): void {
   }
 
 }
+
+function generatePoints(scale: number) {
+  let output = {x: [], y: []};
+  let midpoint = scale*3;
+  let trigon = Math.sqrt(3);
+  // triangle1
+  output.x.push(Math.floor(midpoint));
+  output.x.push(Math.floor(midpoint + scale));
+  output.x.push(Math.floor(midpoint - scale));
+  output.y.push(0);
+  output.y.push(Math.floor(trigon*scale));
+  output.y.push(Math.floor(trigon*scale));
+  // triangle2
+  output.x.push(Math.floor(midpoint - scale));
+  output.x.push(Math.floor(midpoint));
+  output.x.push(Math.floor(midpoint - 2*scale));
+  output.y.push(Math.floor(trigon*scale));
+  output.y.push(Math.floor(trigon*2*scale));
+  output.y.push(Math.floor(trigon*2*scale));
+  // triangle3
+  output.x.push(Math.floor(midpoint - scale));
+  output.x.push(Math.floor(midpoint + scale));
+  output.x.push(Math.floor(midpoint));
+  output.y.push(Math.floor(trigon*scale));
+  output.y.push(Math.floor(trigon*scale));
+  output.y.push(Math.floor(trigon*2*scale));
+  // triangle4
+  output.x.push(Math.floor(midpoint + scale));
+  output.x.push(Math.floor(midpoint + 2*scale));
+  output.x.push(Math.floor(midpoint));
+  output.y.push(Math.floor(trigon*scale));
+  output.y.push(Math.floor(trigon*2*scale));
+  output.y.push(Math.floor(trigon*2*scale));
+  // triangle5
+  output.x.push(Math.floor(midpoint - 2*scale));
+  output.x.push(Math.floor(midpoint - scale));
+  output.x.push(Math.floor(midpoint - 3*scale));
+  output.y.push(Math.floor(trigon*2*scale));
+  output.y.push(Math.floor(trigon*3*scale));
+  output.y.push(Math.floor(trigon*3*scale));
+  // triangle6
+  output.x.push(Math.floor(midpoint - 2*scale));
+  output.x.push(Math.floor(midpoint));
+  output.x.push(Math.floor(midpoint - scale));
+  output.y.push(Math.floor(trigon*2*scale));
+  output.y.push(Math.floor(trigon*2*scale));
+  output.y.push(Math.floor(trigon*3*scale));
+  // triangle7
+  output.x.push(Math.floor(midpoint));
+  output.x.push(Math.floor(midpoint + scale));
+  output.x.push(Math.floor(midpoint - scale));
+  output.y.push(Math.floor(trigon*2*scale));
+  output.y.push(Math.floor(trigon*3*scale));
+  output.y.push(Math.floor(trigon*3*scale));
+  // triangle8
+  output.x.push(Math.floor(midpoint));
+  output.x.push(Math.floor(midpoint + 2*scale));
+  output.x.push(Math.floor(midpoint + scale));
+  output.y.push(Math.floor(trigon*2*scale));
+  output.y.push(Math.floor(trigon*2*scale));
+  output.y.push(Math.floor(trigon*3*scale));
+  // triangle9
+  output.x.push(Math.floor(midpoint + 2*scale));
+  output.x.push(Math.floor(midpoint + 3*scale));
+  output.x.push(Math.floor(midpoint + scale));
+  output.y.push(Math.floor(trigon*2*scale));
+  output.y.push(Math.floor(trigon*3*scale));
+  output.y.push(Math.floor(trigon*3*scale));
+  // return
+  return output;
+}
+
+function getIndices(qty: number) {
+  let output = [];
+  for (let i = 0; i < qty; i++) {
+    output.push(i);
+  }
+  return output;
+}
+
+function generateTransformationStrings(points: any): string[] {
+  let output = [];
+  let degrees = [
+    180, -60, 60,
+    180, -60, 60,
+    120, -120, 0,
+    180, -60, 60,
+    180, -60, 60,
+    120, -120, 0,
+    180, -60, 60,
+    120, -120, 0,
+    180, -60, 60,
+  ];
+  for (let i = 0; i < 27; i++) {
+    output.push(`rotate(${degrees[i]}, ${points.x[i]}, ${points.y[i]})`);
+  }
+  return output;
+}
+
